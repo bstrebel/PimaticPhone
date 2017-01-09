f49174432fe252d4e074b36f9ef97a1a34e515f0
Bernd Strebel <b.strebel@digitec.de>
2017-01-09 15:20:27 +0100

    strict mode settings

--- app/src/main/java/de/digitec/pimatic/phone/MainActivity.java
+++ app/src/main/java/de/digitec/pimatic/phone/MainActivity.java
@@ -1,5 +1,6 @@
 package de.digitec.pimatic.phone;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Enable networking for main thread
+         */
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 }
